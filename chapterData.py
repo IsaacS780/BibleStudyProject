377786e6b1e7917320bf059dcb7dd97f18f8972b
IsaacS780 <isaacsim780@gmail.com>
2026-07-11 17:28:53 -0400

    Add JSON storage for chatper data

---
+++ chapterData.py
@@ -0,0 +1,16 @@
+chapterData = {
+    "book": "1Chronicles",
+    "chapter": 8,
+    "summary": "Benjamin's genealogy continues.",
+    "people": [
+        "Benjamin",
+        "Saul"
+    ],
+    "themes": [
+        "genealogy",
+        "tribes"
+    ]
+}
+
+
+print(chapterData)
